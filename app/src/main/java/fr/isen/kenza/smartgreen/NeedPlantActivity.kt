7945db15d37c1c55ae572d3e86d461238cd6aa12
kenza <kenza.graoudi@isen.yncrea.fr>
2021-05-18 13:38:07 +0200

    humidity

---
+++ app/src/main/java/fr/isen/kenza/smartgreen/NeedPlantActivity.kt
@@ -0,0 +1,11 @@
+package fr.isen.kenza.smartgreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NeedPlantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_need_plant)
+    }
+}
